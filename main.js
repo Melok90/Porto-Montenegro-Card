625c856d3b52dfc0313bd792a0922669368fa0b0
Aleksandr Leontev <al28059@mail.ru>
2023-06-24 02:37:10 +0200

    add swiper

---
+++ main.js
@@ -0,0 +1,30 @@
+AOS.init();
+
+var swiper = new Swiper(".mySwiper", {
+
+    slidesPerView: 1,
+    spaceBetween: 10,
+    // grabCursor: true,
+    loop: true,
+    freeMode: true,
+    pauseOnMouseEnter: false,
+    slideToClickedSlide: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination-2",
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 10,
+      },
+      
+    },
+  });
